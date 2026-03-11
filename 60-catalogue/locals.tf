aec9adb101fc9f817cb49f26ec737b4d7b9b8ddf
venkydevopslearning <venkydevopslearning@gmail.com>
2026-03-12 04:27:13 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  #vpc_id = data.aws_ssm_parameter.vpc_id.value
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   ami_id = data.aws_ami.joindevops.id
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]

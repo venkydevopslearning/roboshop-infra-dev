d1c450ca3c7325e672f5056e44c27327b03186ad
venkydevopslearning <venkydevopslearning@gmail.com>
2026-03-12 01:42:09 +0530

    2

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -5,7 +5,7 @@ environment=$2
 dnf install ansible -y
 
 cd /home/ec2-user
-git clone https://github.com/venkydevopslearning/roboshop-infra-dev.git
+git clone https://github.com/venkydevopslearning/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
 git pull

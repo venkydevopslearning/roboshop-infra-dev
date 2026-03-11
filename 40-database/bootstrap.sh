f98cc66247c5619cd5db7b58d51dc381630f46e7
venkydevopslearning <venkydevopslearning@gmail.com>
2026-03-12 02:07:45 +0530

    fix catalogue issue

--- 40-database/bootstrap.sh
+++ 40-database/bootstrap.sh
@@ -2,11 +2,15 @@
 
 component=$1
 environment=$2
+
 dnf install ansible -y
 
 cd /home/ec2-user
+
+rm -rf ansible-roboshop-roles-tf   # ⭐ IMPORTANT
+
 git clone https://github.com/venkydevopslearning/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
-git pull
+
 ansible-playbook -e component=$component -e env=$environment roboshop.yaml

f84463e9778193ce8a902240e64559587522a6fb
venkydevopslearning <venkydevopslearning@gmail.com>
2026-03-17 17:39:00 +0530

    terraform

--- 90-components/main.tf
+++ 90-components/main.tf
@@ -1,6 +1,6 @@
 module "component" {
     for_each = var.components
-    source = "git::https://github.com/venkydevopslearning/roboshop-infra-dev.git?ref=main"
+    source = "git::https://github.com/venkydevopslearning/terraform-roboshop-component.git?ref=main"
     component = each.key
     rule_priority = each.value.rule_priority
 }
